daeeef8c15d8a7a834ba3ab2e21fdb05c953d210
Saurav Jain <saurav.jain@calsoftinc.com>
2025-06-17 17:06:03 +0530

    monolithic app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from EC2 master code special!');
+  res.send('Hello from EC2 master code special!123');
 });
 
 app.listen(PORT, '0.0.0.0', () => {
